ef8aee29c1d34610083c692b3a6930b0d4f81d4f
Chris <chris@onion>
2021-08-27 22:57:47 +0100

    moved default initialisation from main.cpp

--- src/splat_run.cpp
+++ src/splat_run.cpp
@@ -9,11 +9,6 @@
  */
 
 #include "splat_run.h"
-#include "dem.h"
-#include "itwom3.0.h"
-#include "path.h"
-#include "site.h"
-#include <bzlib.h>
 #include <ctype.h>
 #include <math.h>
 #include <stdio.h>
@@ -26,3 +21,67 @@ using namespace std;
 
 const std::string SplatRun::splat_name = "SPLAT!";
 const std::string SplatRun::splat_version = "2.0-alpha";
+
+//class Foo {
+//public: 
+//  Foo(char x, int y) {}
+//  Foo(int y) : Foo('a', y) {}
+//};
+
+SplatRun::SplatRun() {
+      maxpages = 16;
+      arraysize = -1;
+
+      propagation_model = PROP_ITM;
+      hd_mode = false;
+      coverage = false;
+      LRmap = false;
+      terrain_plot = false;
+      elevation_plot = false;
+      height_plot = false;
+      map = false;
+      longley_plot = false;
+      norm = false;
+      topomap = false;
+      geo = false;
+      kml = false;
+      pt2pt_mode = false;
+      area_mode = false;
+      ngs = false;
+      nolospath = false;
+      nositereports = false;
+      fresnel_plot = true;
+      command_line_log = false;
+      rxsite = false;
+      metric = true;
+      dbm = false;
+      bottom_legend = true;
+      smooth_contours = false;
+
+      altitude = 0.0;
+      altitudeLR = 0.0;
+      tx_range = 0.0;
+      rx_range = 0.0;
+      deg_range = 0.0;
+      deg_limit = 0.0;
+      max_range = 0.0;
+      clutter = 0.0;
+      forced_erp = -1.0;
+      forced_freq = 0.0;
+      fzone_clearance = 0.6;
+      contour_threshold = 0;
+      rx_site.lat = 91.0;
+      rx_site.lon = 361.0;
+      earthradius = EARTHRADIUS;
+
+#ifdef HAVE_LIBPNG
+      imagetype = IMAGETYPE_PNG;
+#else
+      imagetype = IMAGETYPE_PPM;
+#endif
+
+      projection = PROJ_EPSG_4326;
+      multithread = true;
+      verbose = 1;    
+      sdf_delimiter = "_";
+}
